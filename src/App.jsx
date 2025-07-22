ea2e77d3f0194d9f92584edba4205e567d515e71
hakeem.k <hakeem@codesap.com>
2025-07-22 22:25:19 +0530

    Fix vite base path and add render config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import Home from './pages'
@@ -12,17 +12,17 @@ import Contact from './pages/contact'
 import Faq from './pages/faq'
 function App() {  
   return (
-    <Router>
+    <BrowserRouter>
      <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path='/about' element={<About />} />
         <Route path='/product' element={<Prodcut />} />
-        <Route path="faq" element={<Faq />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path='faq' element={<Faq />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
       <Footer/>
-    </Router>
+    </BrowserRouter>
   )
 }
 

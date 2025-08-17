5cd3712f2c21425a44792f5b4e129dcc12a871e4
hakeem <hakeem@codesap.com>
2025-08-17 21:07:35 +0530

    render issue fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './component/header';
-import Footer from './component/footer';
-import ScrollToTop from './component/scrool-totop';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import './index.css';
 import Home from './pages';
+import Header from './component/header';
+import '../src/assets/style/public.css';
+import Footer from './component/footer';
 import About from './pages/about';
-import Product from './pages/prodcut';
-import ProductView from './pages/prodcut-view';
+import Prodcut from './pages/prodcut';
 import Contact from './pages/contact';
 import Faq from './pages/faq';
+import ProdcutView from './pages/prodcut-view';
+import ScrollToTop from './component/scrool-totop';
 import Profile from './component/profile';
-import PageNotFound from './pages/404';
 
 function App() {
   return (
@@ -19,13 +21,12 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/product-view" element={<ProductView />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<PageNotFound />} /> 
+        <Route path='/about' element={<About />} />
+        <Route path='/product' element={<Prodcut />} />
+        <Route path='/product-view' element={<ProdcutView />} />
+        <Route path='/faq' element={<Faq />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
       <Footer />
     </BrowserRouter>

a225f735e9fc90609a65150e9e94c384891f2748
hakeem <hakeem@codesap.com>
2025-08-17 20:59:54 +0530

    render issue fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './component/header';
 import Footer from './component/footer';
-import ScrollToTop from './component/scrool-totop';
+import ScrollToTop from './component/scroll-totop';
 import Home from './pages';
 import About from './pages/about';
-import Product from './pages/prodcut';
-import ProductView from './pages/prodcut-view';
+import Product from './pages/product';
+import ProductView from './pages/product-view';
 import Contact from './pages/contact';
 import Faq from './pages/faq';
 import Profile from './component/profile';
-import PageNotFound from './pages/404';
+import NotFound from './pages/404';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <ScrollToTop />
       <Header />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/product-view" element={<ProductView />} />
+        <Route path="/product/:id" element={<ProductView />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<PageNotFound />} /> 
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-function PageNotFound() {
-   return (
-    <div className="flex flex-col items-center justify-center h-[70vh] text-center">
-      <h1 className="text-5xl font-bold mb-4">404</h1>
-      <p className="text-lg mb-6">Page not found</p>
-      <Link to="/" className="px-4 py-2 bg-blue-600 text-white rounded-lg">
-        Go Home
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-xl mb-8">Oops! Page not found</p>
+      <Link 
+        to="/" className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
+        Return to Home
       </Link>
     </div>
   );
 }
 
-export default PageNotFound
+export default NotFound;

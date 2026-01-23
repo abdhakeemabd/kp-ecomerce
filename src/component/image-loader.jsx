f9d04fdd0d1de3b6dbdef9a3b82d484a0aedc850
hakeem <abdhakeem123abd@gmail.com>
2026-01-23 10:35:19 +0530

    aligment issue fixing

--- src/component/image-loader.jsx
+++ src/component/image-loader.jsx
@@ -6,10 +6,7 @@ const ImageLoader = ({ src, alt, className, width, height, ...props }) => {
     return (
         <div className={`relative overflow-hidden ${className}`} style={{ width: width ? '100%' : 'auto' }}>
             {!isLoaded && (
-                <div
-                    className="skeleton absolute inset-0 z-10 w-full h-full"
-                    style={{ height: height || '100%' }}
-                />
+                <div className="skeleton absolute inset-0 z-10 w-full h-full" style={{ height: height || '100%' }}/>
             )}
             <img
                 src={src}
